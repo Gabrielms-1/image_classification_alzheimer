a63c305efba2d044c2869943824926a8cc0f07d7
gabrielms-1 <gabriel.sousa@syngenta.com>
2025-03-02 15:54:47 -0300

    add network architecture

---
+++ README.md
@@ -0,0 +1,4 @@
+# Image Classification Alzheimer
+
+![AlexNet Architecture](data/notes/alexnet_arch.png)
+![AlexNet Architecture](data/notes/alexNet-architecture.png)
---
+++ src/network.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torch
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes: int = 4):
+        super().__init__()
+        self.features = nn.Sequential(
+            # first conv
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2.0),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+
+            # 2nd conv
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2.0),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+
+            # 3rd, 4th and 5th convs
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes)
+        )
+
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.classifier(x)
+
+        return x
+
+if __name__ == "__main__":
+    # Teste com um tensor de entrada (batch_size=1, 3 canais, 224x224)
+    model = AlexNet(num_classes=4)
+    x = torch.randn(1, 3, 224, 224)
+    output = model(x)
+    print("Saída do modelo:", output.shape)
+
+
+
