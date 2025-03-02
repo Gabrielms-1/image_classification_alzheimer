caa73aa443edeeb58ad5ac607680a7ccff131ff3
gabrielms-1 <gabriel.sousa@syngenta.com>
2025-03-02 18:09:05 -0300

    training model

--- src/train.py
+++ src/train.py
@@ -6,30 +6,48 @@ import os
 import argparse
 import wandb
 
-from dataset import FolderBasedDataset, create_data_loader
+from dataset import FolderBasedDataset, create_data_loaders
 from network import AlexNet
 
 def create_dataloaders(train_dir, val_dir, resize, batch_size):
     train_dataset = FolderBasedDataset(train_dir, resize)
     val_dataset = FolderBasedDataset(val_dir, resize)
 
-    train_dataloader, val_dataloader = create_data_loader(train_dataset, val_dataset, batch_size)
+    train_dataloader, val_dataloader = create_data_loaders(train_dataset, val_dataset, batch_size)
 
     return train_dataloader, val_dataloader
 
+def train_model(model, train_dataloader, val_dataloader, criterion, optimizer, device):
+    
+    model.train()
+
+    for i in range(args.epochs):
+
+        for images, labels, _ in train_dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()    
+    pass
+
 def main(args):
-    wandb.init(
-        project=f"",
-        mode=args.wandb_mode,
-        name=f"",
-        config={
-            "epochs": args.epochs,
-            "batch_size": args.batch_size,
-            "learning_rate": args.learning_rate,
-            "num_classes": args.num_classes,
-            "resize": args.resize
-        }
-    )
+    # wandb.init(
+    #     project=f"",
+    #     name=f"",
+    #     config={
+    #         "epochs": args.epochs,
+    #         "batch_size": args.batch_size,
+    #         "learning_rate": args.learning_rate,
+    #         "num_classes": args.num_classes,
+    #         "resize": args.resize
+    #     }
+    # )
 
     os.makedirs("/opt/ml/checkpoints", exist_ok=True)
 
@@ -43,7 +61,7 @@ def main(args):
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), args.learning_rate, weight_decay=0.0001)
 
-    # train
+    train_model(model, train_dataloader, val_dataloader, criterion, optimizer, device)
 
     return
 
@@ -53,16 +71,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--model_name")
-    parser.add_argument("--epochs")
-    parser.add_argument("--batch_size")
-    parser.add_argument("--learning_rate")
+    parser.add_argument("--epochs", type=int)
+    parser.add_argument("--batch_size", type=int)
+    parser.add_argument("--learning_rate", type=float)
     #parser.add_argument("--task")
     parser.add_argument("--model_dir")
     parser.add_argument("--train")
     parser.add_argument("--val")
-    parser.add_argument("--num_classes")
-    parser.add_argument("--resize")
-    parser.add_argument("--wandbmode")
+    parser.add_argument("--num_classes", type=int)
+    parser.add_argument("--resize", type=int)
 
     args = parser.parse_args()
 
